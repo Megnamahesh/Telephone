72e3c9d1ba5dd516df1c21a6acdb053eb95ae935
Megha H U <megha_h@ETVMM119785.local>
2019-03-01 17:41:35 +0530

    second commit

--- src/Components/Telephone.js
+++ src/Components/Telephone.js
@@ -1,24 +1,21 @@
-import React,{ Component } from 'react';
- 
-class Telephone extends Component{
-  constructor(){
+import React, { Component } from 'react';
+
+class Telephone extends Component {
+  constructor() {
     super();
     this.state = {
       telephonedata: [],
       title: 'Telephone directory application',
       counter: 0,
-
-    
     }
     this.addTodo = this.addTodo.bind(this);
     this.removeTodo = this.removeTodo.bind(this);
-
   }
 
-  removeTodo(index){
+  removeTodo(index) {
     console.log(index);
     let telephonedata = this.state.telephonedata;
-    let data = telephonedata.find(function(data){
+    let data = telephonedata.find(function (data) {
       return data.counter === index;
     })
     telephonedata.splice(data, 1);
@@ -27,14 +24,12 @@ class Telephone extends Component{
     })
   }
 
-
-  addTodo(event){
+  addTodo(event) {
     event.preventDefault();
     let name = this.refs.name.value;
     let phone = this.refs.phone.value;
     let address = this.refs.address.value;
     let counter = this.state.counter;
-
 
     let data = {
       name,
@@ -42,41 +37,38 @@ class Telephone extends Component{
       address,
       counter
     };
-     counter+=1;
+    counter += 1;
     let telephonedata = this.state.telephonedata;
     telephonedata.push(data);
-    this.setState ({
+    this.setState({
       telephonedata: telephonedata,
       counter: counter
     });
-
     this.refs.todoForm.reset();
   }
-  render(){
+  render() {
     let title = this.state.title;
     let telephonedata = this.state.telephonedata;
-    return(
- <div>
- <h1>{title}</h1>
-   <form ref="todoForm">
-      <label>Name</label>
-     <input type="text" ref="name" placeholder="Enter name"/>
-     <label  style={{marginLeft:10}}>Phone No</label>
-     <input type="number" ref="phone" style={{marginLeft:10}} placeholder="Enter Phone no"/>
-     <label  style={{marginLeft:10}}> Address</label>
-     <input type="text" ref="address" style={{marginLeft:10}} placeholder="Enter address"/>
-     <button onClick={this.addTodo} style={{marginLeft:10}}>Add todo </button>
-   </form>
-<ul> 
-{telephonedata.map((data => < li key={data.counter}>{data.name} {data.phone} {data.address}
-<button onClick={this.removeTodo.bind(null,data.counter)}>Remove</button>
-
- </li>))}
-
-</ul>
-  </div>
-);
-}
+    return (
+      <div>
+        <h1>{title}</h1>
+        <form ref="todoForm">
+          <label>Name</label>
+          <input type="text" ref="name" placeholder="Enter name" />
+          <label style={{ marginLeft: 10 }}>Phone No</label>
+          <input type="number" ref="phone" style={{ marginLeft: 10 }} placeholder="Enter Phone no" />
+          <label style={{ marginLeft: 10 }}> Address</label>
+          <input type="text" ref="address" style={{ marginLeft: 10 }} placeholder="Enter address" />
+          <button onClick={this.addTodo} style={{ marginLeft: 10 }}>Add todo </button>
+        </form>
+        <ul>
+          {telephonedata.map((data => < li key={data.counter}>{data.name} {data.phone} {data.address}
+            <button onClick={this.removeTodo.bind(null, data.counter)}>Remove</button>
+          </li>))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Telephone;
